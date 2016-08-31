9a2b87f8e0580d7b53b29d41fba66fd5fc49d3b6
Smith Jeremy <smith@novius.fr>
2016-08-31 16:33:40 +0200

    rename

--- Boom/Helper/App.php
+++ Boom/Helper/App.php
@@ -2,72 +2,73 @@
 
 namespace Boom\Helper;
 
-class App {
-    public static function exist($appname)
-    {
-        return (is_dir('./Apps/'.ucfirst($appname)));
-    }
+class App
+{
+	public static function exist($appname)
+	{
+		return (is_dir('./Apps/' . ucfirst($appname)));
+	}
 
-    public static function getConfig($appname)
-    {
-        $appname = ucfirst($appname);
-        $config = require_once "./Apps/" . $appname . '/Config/' . $appname . '.php' ;
+	public static function getConfig($appname)
+	{
+		$appname = ucfirst($appname);
+		$config = require_once "./Apps/" . $appname . '/Config/' . $appname . '.php';
 
-        return $config;
-    }
+		return $config;
+	}
 
-    public static function isAppCtrl($appname, $ctrlname)
-    {
-        $ctrlname = ucfirst($ctrlname);
-        $namespace = '\Apps\\' . $appname . '\Ctrl\\' . $ctrlname;
+	public static function isAppCtrl($appname, $ctrlname)
+	{
+		$ctrlname = ucfirst($ctrlname);
+		$namespace = '\Apps\\' . $appname . '\Ctrl\\' . $ctrlname;
 
-        return class_exists($namespace);
-    }
+		return class_exists($namespace);
+	}
 
-    public static function isCtrlAction($appname, $ctrl, $action)
-    {
-        $namespace = 'Apps\\' . $appname . '\Ctrl\\' . $ctrl;
+	public static function isCtrlAction($appname, $ctrl, $action)
+	{
+		$namespace = 'Apps\\' . $appname . '\Ctrl\\' . $ctrl;
 
-        return method_exists($namespace, 'action_'.$action);
-    }
+		return method_exists($namespace, 'action_' . $action);
+	}
 
-    public static function getCtrlFromRoute($appname, &$params)
-    {
-        $appname = ucfirst($appname);
-        $config = self::getConfig($appname);
-        $ctrl = $appname;
+	public static function getCtrlFromRoute($appname, &$params)
+	{
+		$appname = ucfirst($appname);
+		$config = self::getConfig($appname);
+		$ctrl = $appname;
 
-        if (isset($params[0])) {
-            if (self::isAppCtrl($appname, $params[0])) {
-                $ctrl = $params[0];
-                array_shift($params);
-            } elseif (isset($config['default_ctrl']) && !empty($config['default_ctrl'])){
-                $ctrl = $config['default_ctrl'];
-            }
-        }
+		if (isset($params[0])) {
+			if (self::isAppCtrl($appname, $params[0])) {
+				$ctrl = $params[0];
+				array_shift($params);
+			} elseif (isset($config['default_ctrl']) && !empty($config['default_ctrl'])) {
+				$ctrl = $config['default_ctrl'];
+			}
+		}
 
-        return ucfirst($ctrl);
-    }
+		return ucfirst($ctrl);
+	}
 
-    public static function getActionFromRoute($appname, $ctrl, &$params)
-    {
-        $appname = ucfirst($appname);
-        $config = self::getConfig($appname);
-        $action = 'main';
+	public static function getActionFromRoute($appname, $ctrl, &$params)
+	{
+		$appname = ucfirst($appname);
+		$config = self::getConfig($appname);
+		$action = 'main';
 
-        if (isset($params[0])) {
-            if (self::isCtrlAction($appname, $ctrl, $params[0])) {
-                $action = $params[0];
-                array_shift($params);
-            } elseif (isset($config['default_action']) && !empty($config['default_action'])){
-                $action = $config['default_action'];
-            }
-        }
+		if (isset($params[0])) {
+			if (self::isCtrlAction($appname, $ctrl, $params[0])) {
+				$action = $params[0];
+				array_shift($params);
+			} elseif (isset($config['default_action']) && !empty($config['default_action'])) {
+				$action = $config['default_action'];
+			}
+		}
 
-        return 'action_'.$action;
-    }
+		return 'action_' . $action;
+	}
 
-	public static function getEnhancersConfig($appname)
+	public static function getAllEnhancersConfig($appname)
 	{
 		$config = self::getConfig($appname);
 
@@ -83,7 +84,8 @@ class App {
 		if (isset($config['enhancers'])
 			&& !empty($config['enhancers'])
 			&& isset($config['enhancers'][$enhancername])
-			&& !empty($config['enhancers'][$enhancername])) {
+			&& !empty($config['enhancers'][$enhancername])
+		) {
 			return $config['enhancers'][$enhancername];
 		} else {
 			echo "Enhancer $enhancername doesn't exit!";
