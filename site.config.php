74d6c0d8e9200dc98c1084103859f22c2b14b84e
SwithFr <contact@swith.fr>
2016-09-20 13:13:16 +0200

    db conf

--- site.config.php
+++ site.config.php
@@ -10,7 +10,8 @@ $config['db']['user']   = "user";
 $config['db']['pass']   = "password";
 $config['db']['dbname'] = "exampleapp";
 */
-define('DB_HOST', '0.0.0.0');
+
+define('DB_HOST', 'localhost');
 define('DB_USER', 'boom_cms');
 define('DB_PASS', 'boom_cms');
 define('DB_NAME', 'boom_cms');
