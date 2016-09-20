9744f506f5132e84df99c04c60b7035ff86fcb53
SwithFr <contact@swith.fr>
2016-09-20 13:09:37 +0200

    left bar content

--- Apps/Admin/Views/Leftbar.php
+++ Apps/Admin/Views/Leftbar.php
@@ -1,7 +1,7 @@
 <div class="leftbar">
     <ul>
-    <?php foreach($params['apps'] as $app): ?>
-        <li><a href="<?=BASE_URL?>admin/listing/<?= $app['name'] ?>"><?= $app['name'] ?></a></li>
-    <?php endforeach; ?>
+        <?php foreach($apps as $app): ?>
+            <li><a href="<?=BASE_URL?>admin/listing/<?= $app['name'] ?>"><?= $app['name'] ?></a></li>
+        <?php endforeach; ?>
     </ul>
 </div>
