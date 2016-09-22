cee839a9947745ffdccd03aaac3e9c060c1bb742
SwithFr <contact@swith.fr>
2016-09-22 13:50:15 +0200

    add connect view

---
+++ Apps/Users/Views/Connect.php
@@ -0,0 +1,6 @@
+<form action="" method="post">
+    <label for="login"><?= __('Login') ?></label>
+    <input type="text" name="login" id="login">
+    <label for="password"><?= __('Mot de passe') ?></label>
+    <input type="password" name="password" id="password">
+</form>
