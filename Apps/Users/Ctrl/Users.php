fae4b47e658539a2ad256daf14e54bc60900c312
SwithFr <contact@swith.fr>
2016-09-22 20:08:21 +0200

    connect logic

--- Apps/Users/Ctrl/Users.php
+++ Apps/Users/Ctrl/Users.php
@@ -5,13 +5,28 @@ namespace Apps\Users\Ctrl;
 
 
 use Boom\Ctrl\Controller;
+use Boom\Helper\Session;
+use Boom\Helper\Token;
 
 class Users extends Controller
 {
 
     public function action_connect($params = null)
     {
-        $this->view('Connect');
+        $user = $this->User->forge();
+        if ($this->request->isPost()) {
+            $user = $this->User->forge($this->request->getParsedBody());
+            if ($user = $user->authentify()) {
+                $token = Token::generate($user);
+            	Session::set("token", $token);
+                $user->token = $token;
+                $user->save();
+            } else {
+                var_dump("MOINS COOL");die();
+            }
+        }
+
+        $this->view('Connect', compact('user'));
     }
 
     public function action_logout($params = null)
