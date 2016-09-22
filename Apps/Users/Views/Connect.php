95327bdee71736564bd3507b541eefec5e4dbbff
SwithFr <contact@swith.fr>
2016-09-22 20:09:23 +0200

    add submit input

--- Apps/Users/Views/Connect.php
+++ Apps/Users/Views/Connect.php
@@ -3,4 +3,5 @@
     <input type="text" name="login" id="login">
     <label for="password"><?= __('Mot de passe') ?></label>
     <input type="password" name="password" id="password">
+    <input type="submit" value="<?= __('Me connecter') ?>">
 </form>
