c70ba5922b01d33915bdd062b31620a82ed9cf8e
SwithFr <contact@swith.fr>
2016-09-22 13:49:41 +0200

    add users crud

---
+++ Apps/Users/Cruds/User.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'firstname' => array(
+        'label' => __('Prénom'),
+        'type' => 'text'
+    ),
+    'lastname' => array(
+        'label' => __('Nom'),
+        'type' => 'text'
+    ),
+    'login' => array(
+        'label' => __('Login'),
+        'type' => 'text'
+    ),
+    'password' => array(
+        'label' => __('Mot de passe'),
+        'type' => 'password'
+    ),
+];
