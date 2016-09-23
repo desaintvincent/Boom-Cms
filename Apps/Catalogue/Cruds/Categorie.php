e6e2ea210e762080f1cbee48f8d5b4f3544290ce
SwithFr <contact@swith.fr>
2016-09-23 08:49:30 +0200

    add categori crud

--- Apps/Catalogue/Cruds/Categorie.php
+++ Apps/Catalogue/Cruds/Categorie.php
@@ -1,2 +1,12 @@
 <?php
 
+return [
+    'cat_title' => array(
+        'label' => 'Le titre',
+        'type' => 'text'
+    ),
+    'cat_content' => array(
+        'label' => 'Le contenu',
+        'type' => 'wysiwyg'
+    ),
+];
