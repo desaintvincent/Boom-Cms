ccf7aa5275d808db60fa9c6aaa787fbb92494419
SwithFr <contact@swith.fr>
2016-09-23 13:55:42 +0200

    authentify method

--- Apps/Users/Model/User.php
+++ Apps/Users/Model/User.php
@@ -5,6 +5,8 @@ namespace Apps\Users\Model;
 
 
 use Boom\Helper\Security;
+use Boom\Helper\Session;
+use Boom\Helper\Token;
 use Boom\Model\Model;
 
 class User extends  Model
@@ -19,7 +21,13 @@ class User extends  Model
         ]);
 
         if ($user && $user->password === Security::crypt($this->password)) {
-        	return $user;
+            $token = Token::generate($user);
+            Session::set("token", $token);
+            $user->token = $token;
+        	$userConnected = $this->forge((array) $user);
+            $userConnected->save();
+
+            return true;
         }
 
         return false;
