00b888f48467102e5166795255fff99582ec3e7e
stvince <destvincent@hotmail.fr>
2016-09-27 23:36:25 +0200

    add bdd

--- boom_cms.sql
+++ boom_cms.sql
@@ -1,129 +1,224 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Hôte: 127.0.0.1 (MySQL 5.5.42)
-# Base de données: boom_cms
-# Temps de génération: 2016-09-23 11:36:29 +0000
-# ************************************************************
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mar 27 Septembre 2016 à 23:35
+-- Version du serveur :  5.7.13-0ubuntu0.16.04.2
+-- Version de PHP :  7.0.8-0ubuntu0.16.04.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `boom_cms`
+--
 
-# Affichage de la table categories
-# ------------------------------------------------------------
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `categories`;
+--
+-- Structure de la table `categories`
+--
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `cate_title` varchar(255) NOT NULL,
   `cate_slug` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+--
+-- Contenu de la table `categories`
+--
 
-INSERT INTO `categories` (`id`, `cate_title`, `cate_slug`, `content`)
-VALUES
-	(1,'cat1','cat1','je suis la cat1');
+INSERT INTO `categories` (`id`, `cate_title`, `cate_slug`, `content`) VALUES
+(1, 'cat1', 'cat1', 'je suis la cat1');
 
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `menus`
+--
 
-# Affichage de la table pages
-# ------------------------------------------------------------
+CREATE TABLE `menus` (
+  `id` int(11) NOT NULL,
+  `menu_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `pages`;
+--
+-- Contenu de la table `menus`
+--
+
+INSERT INTO `menus` (`id`, `menu_title`) VALUES
+(1, 'menu principaleuh');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `menu_items`
+--
+
+CREATE TABLE `menu_items` (
+  `id` int(11) NOT NULL,
+  `mitem_parent_id` int(11) DEFAULT NULL,
+  `mitem_display_order` int(11) NOT NULL,
+  `mitem_menu_id` int(11) NOT NULL,
+  `mitem_title` varchar(255) NOT NULL,
+  `mitem_arg` varchar(255) DEFAULT NULL,
+  `mitem_type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='`id`, `name`, `parent_id`, `display_order`';
+
+--
+-- Contenu de la table `menu_items`
+--
+
+INSERT INTO `menu_items` (`id`, `mitem_parent_id`, `mitem_display_order`, `mitem_menu_id`, `mitem_title`, `mitem_arg`, `mitem_type`) VALUES
+(4, NULL, 1, 1, 'aaaaa', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pages`
+--
 
 CREATE TABLE `pages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  UNIQUE KEY `id` (`id`)
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-LOCK TABLES `pages` WRITE;
-/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
+--
+-- Contenu de la table `pages`
+--
 
-INSERT INTO `pages` (`id`, `title`, `slug`, `content`)
-VALUES
-	(1,'page1','page1','&lt;p&gt;debut enhancer 1&lt;/p&gt;\r\n&lt;p&gt;&lt;enhancer class=&quot;noneditable list_categories&quot; data-params=&quot;{&amp;quot;appname&amp;quot;:&amp;quot;catalogue&amp;quot;,&amp;quot;name&amp;quot;:&amp;quot;Liste les cat\\u00e9gories&amp;quot;,&amp;quot;controller&amp;quot;:&amp;quot;categories&amp;quot;,&amp;quot;action&amp;quot;:&amp;quot;categories&amp;quot;}&quot;&gt;Liste les cat&amp;eacute;gories&lt;/enhancer&gt;&lt;/p&gt;\r\n&lt;p&gt;fin enhancer 1&lt;/p&gt;\r\n&lt;p&gt;&amp;nbsp;&lt;/p&gt;\r\n&lt;p&gt;debut enhancer 2&lt;/p&gt;\r\n&lt;p&gt;&lt;enhancer class=&quot;noneditable list_products_category&quot; data-params=&quot;{&amp;quot;appname&amp;quot;:&amp;quot;catalogue&amp;quot;,&amp;quot;name&amp;quot;:&amp;quot;Liste les produits d&#039;une cat\\u00e9gorie&amp;quot;,&amp;quot;controller&amp;quot;:&amp;quot;categories&amp;quot;,&amp;quot;action&amp;quot;:&amp;quot;main&amp;quot;}&quot;&gt;Liste les produits d&#039;une cat&amp;eacute;gorie&lt;/enhancer&gt;&lt;/p&gt;\r\n&lt;p&gt;fin enhancer 2&lt;/p&gt;\r\n&lt;p&gt;&amp;nbsp;&lt;/p&gt;'),
-	(2,'page2','page2','&lt;p&gt;&lt;enhancer class=&quot;noneditable list_products_category&quot; data-params=&quot;{&amp;quot;appname&amp;quot;:&amp;quot;catalogue&amp;quot;,&amp;quot;name&amp;quot;:&amp;quot;Liste les produits d&#039;une cat\\u00e9gorie&amp;quot;,&amp;quot;controller&amp;quot;:&amp;quot;categories&amp;quot;,&amp;quot;action&amp;quot;:&amp;quot;main&amp;quot;}&quot;&gt;Liste les produits d&#039;une cat&amp;eacute;gorie&lt;/enhancer&gt;&lt;/p&gt;');
+INSERT INTO `pages` (`id`, `title`, `slug`, `content`) VALUES
+(1, 'page1', 'page1', '&lt;p&gt;debut enhancer 1&lt;/p&gt;\r\n&lt;p&gt;&lt;enhancer class=&quot;noneditable list_categories&quot; data-params=&quot;{&amp;quot;appname&amp;quot;:&amp;quot;catalogue&amp;quot;,&amp;quot;name&amp;quot;:&amp;quot;Liste les cat\\u00e9gories&amp;quot;,&amp;quot;controller&amp;quot;:&amp;quot;categories&amp;quot;,&amp;quot;action&amp;quot;:&amp;quot;categories&amp;quot;}&quot;&gt;Liste les cat&amp;eacute;gories&lt;/enhancer&gt;&lt;/p&gt;\r\n&lt;p&gt;fin enhancer 1&lt;/p&gt;\r\n&lt;p&gt;&amp;nbsp;&lt;/p&gt;\r\n&lt;p&gt;debut enhancer 2&lt;/p&gt;\r\n&lt;p&gt;&lt;enhancer class=&quot;noneditable list_products_category&quot; data-params=&quot;{&amp;quot;appname&amp;quot;:&amp;quot;catalogue&amp;quot;,&amp;quot;name&amp;quot;:&amp;quot;Liste les produits d&#039;une cat\\u00e9gorie&amp;quot;,&amp;quot;controller&amp;quot;:&amp;quot;categories&amp;quot;,&amp;quot;action&amp;quot;:&amp;quot;main&amp;quot;}&quot;&gt;Liste les produits d&#039;une cat&amp;eacute;gorie&lt;/enhancer&gt;&lt;/p&gt;\r\n&lt;p&gt;fin enhancer 2&lt;/p&gt;\r\n&lt;p&gt;&amp;nbsp;&lt;/p&gt;'),
+(2, 'page2', 'page2', '&lt;p&gt;&lt;enhancer class=&quot;noneditable list_products_category&quot; data-params=&quot;{&amp;quot;appname&amp;quot;:&amp;quot;catalogue&amp;quot;,&amp;quot;name&amp;quot;:&amp;quot;Liste les produits d&#039;une cat\\u00e9gorie&amp;quot;,&amp;quot;controller&amp;quot;:&amp;quot;categories&amp;quot;,&amp;quot;action&amp;quot;:&amp;quot;main&amp;quot;}&quot;&gt;Liste les produits d&#039;une cat&amp;eacute;gorie&lt;/enhancer&gt;&lt;/p&gt;');
 
-/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
-
-# Affichage de la table products
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `products`;
+--
+-- Structure de la table `products`
+--
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `prod_title` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `prod_category_id` int(1) NOT NULL,
-  UNIQUE KEY `id` (`id`)
+  `prod_category_id` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+--
+-- Contenu de la table `products`
+--
 
-INSERT INTO `products` (`id`, `prod_title`, `slug`, `content`, `prod_category_id`)
-VALUES
-	(1,'produit1','produit1','&lt;p&gt;sqdsd&lt;/p&gt;',1);
+INSERT INTO `products` (`id`, `prod_title`, `slug`, `content`, `prod_category_id`) VALUES
+(1, 'produit1', 'produit1', '&lt;p&gt;sqdsd&lt;/p&gt;', 1);
 
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
-
-# Affichage de la table users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
+--
+-- Structure de la table `users`
+--
 
 CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `login` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL DEFAULT '',
-  `token` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `token` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+--
+-- Contenu de la table `users`
+--
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `login`, `password`, `token`)
-VALUES
-	(1,'admin','admin','admin','90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad','0617c750ecb985b048b8b3b2dd9c42520faab6e4');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `login`, `password`, `token`) VALUES
+(1, 'admin', 'admin', 'admin', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', '25065e12be6e15f6666873fb290eb343dc5e5db5');
 
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Index pour les tables exportées
+--
 
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
+--
+-- Index pour la table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`);
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+--
+-- Index pour la table `menu_items`
+--
+ALTER TABLE `menu_items`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `pages`
+--
+ALTER TABLE `pages`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `menu_items`
+--
+ALTER TABLE `menu_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
