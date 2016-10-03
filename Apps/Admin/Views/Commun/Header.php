8e41e3d940eaca257a96bf3b6648454fb1261242
destvincent <destvincent@novius.fr>
2016-10-03 18:29:54 +0200

    aa

--- Apps/Admin/Views/Commun/Header.php
+++ Apps/Admin/Views/Commun/Header.php
@@ -1,4 +1,4 @@
-<div class="header">
+<nav class="header" data-spy="affix" data-offset-top="50">
     <div class="logo">
         <a href="<?=BASE_URL?>admin">
             <img src="<?=BASE_URL?>Apps/Admin/Static/dist/logo.svg" alt="Logo de Boom" width="100" height="27">
@@ -8,4 +8,4 @@
     <form class="form-signin" method="post">
         <button class="btn btn-primary expanded logout" type="submit" name="deconnexion">Deconnexion</button>
     </form>
-</div>
+</nav>
