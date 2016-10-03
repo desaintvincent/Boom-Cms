64cc30cfe824ff6e53c51bfd8983e902d55e7bc6
destvincent <destvincent@novius.fr>
2016-10-03 14:05:01 +0200

    css form

--- Boom/Helper/Crud.php
+++ Boom/Helper/Crud.php
@@ -22,18 +22,19 @@ class Crud
                 error('Type ' .$item['type']. ' doesn\'t exist! (yet)');
             }
         }
-        $html .= '<div class="save"><button class="button" type="submit">Enregistrer les informations</button></div>';
+        $html .= '<div class="save"><button type="button" class="btn btn-primary" type="submit">Enregistrer les informations</button></div>';
         $html .= '</form>';
 
         return $html;
     }
 
+    ///@todo mettre template
     public static function input_wysiwyg($key, $item)
     {
         $html = "<div class='{$item['type']}-field'>
-            <label>{$item['label']} :
-                <textarea class='wysiwyg' name='{$key}' id='{$key}'>".self::$item[$key]."</textarea>
-            </label>
+            <div class='sub-title'>{$item['label']}</div>
+            <textarea class='wysiwyg' name='{$key}' id='{$key}'>".self::$item[$key]."</textarea>
+
         </div>";
         return $html;
     }
@@ -41,8 +42,8 @@ class Crud
     public static function input_text($key, $item)
     {
         $html = "<div class='{$item['type']}-field'>
-                    <label for='{$key}'>{$item['label']}</label>
-                    <input type='text' value='" . self::$item[$key] . "' name='{$key}' id='{$key}' " . self::required($item) . ">
+                    <div class='sub-title'>{$item['label']}</div>
+                    <input type='text' class='form-control' value='" . self::$item[$key] . "' name='{$key}' id='{$key}' " . self::required($item) . ">
                 </div>";
         return $html;
     }
@@ -50,8 +51,8 @@ class Crud
     public static function input_password($key, $item)
     {
         $html = "<div class='{$item['type']}-field'>
-                    <label for='{$key}'>{$item['label']}</label>
-                    <input type='password' value='" . self::$item[$key] . "' name='{$key}' id='{$key}' " . self::required($item) . ">
+                    <div class='sub-title'>{$item['label']}</div>
+                    <input type='password' class='form-control' value='" . self::$item[$key] . "' name='{$key}' id='{$key}' " . self::required($item) . ">
                 </div>";
         return $html;
     }
@@ -59,25 +60,26 @@ class Crud
     public static function input_email($key, $item)
     {
         $html = "<div class='{$item['type']}-field'>
-                    <label for='{$key}'>{$item['label']}</label>
-                    <input type='email' value='" . self::$item[$key] . "' name='{$key}' id='{$key}' " . self::required($item) . ">
+                    <div class='sub-title'>{$item['label']}</div>
+                    <input type='email' class='form-control' value='" . self::$item[$key] . "' name='{$key}' id='{$key}' " . self::required($item) . ">
                 </div>";
         return $html;
     }
 
+    ///@todo mettre template
     public static function input_textarea($key, $item)
     {
         $html = "<div class='{$item['type']}-field'>
-            <label>{$item['label']} :
+            <div class=\"sub-title\">{$item['label']}</div>
                 <textarea name='{$key}' id='{$key}'>".self::$item[$key]."</textarea>
-            </label>
         </div>";
         return $html;
     }
 
+    ///@todo mettre template
     public static function input_select($key, $item) {
         $html = "<div class='{$item['type']}-field'>
-                    <label>{$item['label']} :
+                    <div class='sub-title'>{$item['label']}</div>
                         <select name='{$key}' id='{$key}'>";
 
         foreach ($item['value'] as $kval => $val) {
@@ -88,14 +90,13 @@ class Crud
             }
         }
         $html .= "</select>
-            </label>
         </div>";
 
         return $html;
     }
 
     public static function input_checkbox($key, $item) {
-        $html = "<div class='{$item['type']}-field'>
+        $html = "<div class='checkbox3 checkbox-inline checkbox-check checkbox-light'>
                     <input type='checkbox' name='{$key}' id='{$key}' " . (self::$item[$key] ? "checked " : " ") . self::required($item) . ">
                     <label for='{$key}'>{$item['label']}</label>
                 </div>";
