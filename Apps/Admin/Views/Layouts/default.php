2fd72f358890aadd479f4ed2a10c3fa8f4786b19
destvincent <destvincent@novius.fr>
2016-10-03 13:42:07 +0200

    leftbar sur toute la hauteur

--- Apps/Admin/Views/Layouts/default.php
+++ Apps/Admin/Views/Layouts/default.php
@@ -1,14 +1,17 @@
 <?php
 echo \Apps\Admin\Ctrl\Admin::view_static('Commun/Head', $params);
-echo \Apps\Admin\Ctrl\Admin::view_static('Commun/Header', $params);
 echo \Apps\Admin\Ctrl\Admin::view_static('Commun/Leftbar', ['apps' => $apps]);
 ?>
 <div class="content">
-    <div class="row">
-        <div class="col-lg-12">
-            <?= $tampon ?>
+    <?= \Apps\Admin\Ctrl\Admin::view_static('Commun/Header', $params) ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <?= $tampon ?>
+            </div>
         </div>
     </div>
+
 </div>
 <?php
 echo \Apps\Admin\Ctrl\Admin::view_static('Commun/Foot', array_merge($params, ['enhancers' => $enhancers]));
