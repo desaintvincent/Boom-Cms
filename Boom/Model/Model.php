ed6d825ec65f89b98ccfc5c2913947439f12b8fc
stvince <destvincent@hotmail.fr>
2016-11-10 15:38:55 +0100

    files

--- Boom/Model/Model.php
+++ Boom/Model/Model.php
@@ -6,10 +6,25 @@ use Cake\Datasource\EntityInterface;
 use Cake\Event\Event;
 use Cake\ORM\Table;
 
+define('TARGET', 'Static/img/cms/');    // Repertoire cible
+define('MAX_SIZE', 100000);    // Taille max en octets du fichier
+define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
+define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
+
 class Model extends Table
 {
     public function beforeMarshal(Event $event, \ArrayObject $data, \ArrayObject $options) {
-        d($_FILES);
+        $tabExt = array('jpg','gif','png','jpeg');
+        foreach ($_FILES as $key => $file) {
+            if (empty($file['name'])) {
+                //$data->{$key} = null; yoloooo
+            } else {
+                $extension  = pathinfo($_FILES[$key]['name'], PATHINFO_EXTENSION);
+                $nomImage = $key .'.'. $extension;
+                move_uploaded_file($_FILES[$key]['tmp_name'], TARGET.$nomImage);
+                $_POST[$key] = TARGET.$nomImage;
+            }
+        }
     }
 
     public function beforeSave(Event $event, EntityInterface $entity, $options)
